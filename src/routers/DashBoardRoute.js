b339e5f6a1563969bf5d1e26821ebc6f122552e4
Nebura <hamklieneduprofesional@gmail.com>
2021-03-25 22:17:18 -0600

    Se modifico la ruta de la pantalla sobre consulta.

--- src/routers/DashBoardRoute.js
+++ src/routers/DashBoardRoute.js
@@ -2,10 +2,10 @@ import React                                                from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { NavBar }                                           from '../components/ui/NavBar';
 import { AboutUsScreen }                                    from '../screens/AboutUsScreen';
-import { CheckOutForm }                                     from '../components/form/CheckOutScreen';
 import { ProductScreen }                                    from '../components/product/ProductScreen';
 import { CartScreen }                                       from '../components/cart/CartScreen';
 import { SearchProduct }                                    from '../components/product/SearchProduct';
+import { AskScreen }                                    from '../components/form/AskScreen';
 import { FAQScreen }                                        from '../screens/FAQScreen';
 
 export const DashBoardRoute = () => {
@@ -17,7 +17,7 @@ export const DashBoardRoute = () => {
                     <Route exact path="/productos"               component={ SearchProduct } />
                     <Route exact path= "/productos/:productName" component={ ProductScreen } />
                     <Route exact path="/preguntas"               component={ FAQScreen }     />
-                    <Route exact path="/contactos"               component={ CheckOutForm }  />
+                    <Route exact path="/contactos"               component={ AskScreen }  />
                     <Route exact path="/nosotros"                component={ AboutUsScreen } />
                     <Route exact path="/carrito"                 component={ CartScreen }    />
                     <Redirect to="/productos"/>
